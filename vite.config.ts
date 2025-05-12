fecbf88e1a1a442e50d0d9224cfe0165bf4ef543
Raiden Ishigami <contact.raidenishi69@gmail.com>
2025-05-12 20:37:14 -0300

    raidenprjkts: remove base from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/raidenprjkts.github.io/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
